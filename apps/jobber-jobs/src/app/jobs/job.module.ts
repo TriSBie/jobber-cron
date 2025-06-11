28b63b02668906e048b27e2476b65ae834bb4a28
KrissTiNa207 <99896326+TriSBie@users.noreply.github.com>
2025-06-11 10:39:11 +0700

    build: addon nx configuration with ts-proto-type

--- apps/jobber-jobs/src/app/jobs/job.module.ts
+++ apps/jobber-jobs/src/app/jobs/job.module.ts
@@ -3,10 +3,21 @@ import { Module } from "@nestjs/common";
 import { JobResolver } from "./job.resolver";
 import { JobsService } from "./job.service";
 import { FibonacciJob } from "./job/fibonacci.job";
-
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { AUTH_PACKAGE_NAME } from "types/auth";
+import { join } from "path";
 
 @Module({
-    imports: [DiscoveryModule,],
+    imports: [DiscoveryModule, ClientsModule.register([
+        {
+            name: AUTH_PACKAGE_NAME,
+            transport: Transport.GRPC,
+            options: {
+                package: AUTH_PACKAGE_NAME,
+                protoPath: join(__dirname, 'proto/auth.proto'),
+            }
+        }
+    ])],
     providers: [JobsService, JobResolver, FibonacciJob]
 })
 export class JobModule { }
--- apps/jobber/src/main.ts
+++ apps/jobber/src/main.ts
@@ -3,12 +3,14 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { GrpcOptions, Transport } from '@nestjs/microservices';
 import * as cookieParser from 'cookie-parser';
+import { join } from 'path';
+import { AppModule } from './app/app.module';
+import { AUTH_PACKAGE_NAME } from 'types/auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,6 +26,14 @@ async function bootstrap() {
   });
   const configService = app.get(ConfigService);
 
+  app.connectMicroservice<GrpcOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: AUTH_PACKAGE_NAME,
+      protoPath: join(__dirname, 'proto/auth.proto'),
+    },
+  });
+  await app.startAllMicroservices();
   const port = configService.getOrThrow('PORT');
   await app.listen(port);
   Logger.log(
